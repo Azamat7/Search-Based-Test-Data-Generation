2f5ac4141f87c87339b0152bafca618ec426dd63
Azamat Smagulov <s.a.smagulov@gmail.com>
2019-05-06 12:31:41 +0900

    todos created

--- target.py
+++ target.py
@@ -1,11 +1,11 @@
 import math
 
 def getSquareRounded(a):
- 	ceil = 100
- 	ans = a * a 
- 	if ans>ceil:
- 		return ceil
- 	return ans
+	ceil = 100
+	ans = a * a 
+	if ans>ceil:
+		return ceil
+	return ans
 
 def getAbsoluteDifference(a,b):
 	if a>b:
